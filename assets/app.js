5da12a2cac5ed410c410de36ca56ad038980048b
jmaurojr <112908979+jmaurojr@users.noreply.github.com>
2023-01-30 09:12:24 -0300

    fix details

--- assets/app.js
+++ assets/app.js
@@ -69,9 +69,9 @@ function displayWeather(data) {
   weatherIcon.src = `./assets/${icon}.svg`;
   weatherDescription.textContent = description;
   currentTemperature.textContent = `${Math.round(temp)}ºC`;
-  windSpeed.textContent = `${Math.round(speed * 3.6)}Km/h`;
-  feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`;
-  currentHumidity.textContent = `${humidity}%`;
+  windSpeed.textContent = `${Math.round(speed * 3.6)} Km/h`;
+  feelsLikeTemperature.textContent = `${Math.round(feels_like)} ºC`;
+  currentHumidity.textContent = `${humidity} %`;
   sunriseTime.textContent = formatTime(sunrise);
   sunsetTime.textContent = formatTime(sunset);
 }
